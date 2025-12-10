e7d60910a00455169b50ec73581e5ece6088428e
Adeniran Ádám Kristóf <90899862+Adeniran24@users.noreply.github.com>
2025-12-10 11:56:09 +0100

    Domain osztály paramétereinek felvétele és konstruktora

--- Domain/DomainEntry.cs
+++ Domain/DomainEntry.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Domain
 {
-    internal class DomainEntry
+    public class DomainEntry
     {
+        public string DomainName { get; private set; }
+        public string IpAddress { get; private set; }
+
+
+        public DomainEntry(string domainName, string ipAddress)
+        {
+            DomainName = domainName;
+            IpAddress = ipAddress;
+        }
     }
 }
